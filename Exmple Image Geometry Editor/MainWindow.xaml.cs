a3cc5f87b90f6882d16a717acd2c3d0bd466e797
Сергей Голованчиков <golovan4ickoff@gmail.com>
2026-01-30 15:54:22 +0700

    initial commit

---
+++ Exmple Image Geometry Editor/MainWindow.xaml.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
+using Emgu.CV.Structure;
+using Emgu.CV;
+
+namespace Exmple_Image_Geometry_Editor
+{
+    public partial class MainWindow : Window
+    {
+        private Image<Bgr, byte> sourceImage;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public BitmapSource ToBitmapSource(Image<Bgr, byte> image)
+        {
+            var mat = image.Mat;
+
+            return BitmapSource.Create(
+                mat.Width,
+                mat.Height,
+                96d,
+                96d,
+                PixelFormats.Bgr24,
+                null,
+                mat.DataPointer,
+                mat.Step * mat.Height,
+                mat.Step);
+        }
+        public Image<Bgr, byte> ToEmguImage(BitmapSource source)
+        {
+            if (source == null) return null;
+
+            FormatConvertedBitmap safeSource = new FormatConvertedBitmap();
+            safeSource.BeginInit();
+            safeSource.Source = source;
+            safeSource.DestinationFormat = PixelFormats.Bgr24;
+            safeSource.EndInit();
+
+            Image<Bgr, byte> resultImage = new Image<Bgr, byte>(safeSource.PixelWidth, safeSource.PixelHeight);
+            var mat = resultImage.Mat;
+
+            safeSource.CopyPixels(
+                new System.Windows.Int32Rect(0, 0, safeSource.PixelWidth, safeSource.PixelHeight),
+                mat.DataPointer,
+                mat.Step * mat.Height,
+                mat.Step);
+
+            return resultImage;
+        }
+
+        private void LoadImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Файлы изображений (*.jpg, *.jpeg, *.png)|*.jpg;*.jpeg;*.png";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                sourceImage = new Image<Bgr, byte>(openFileDialog.FileName);
+
+                MainImage.Source = ToBitmapSource(sourceImage);
+            }
+        }
+
+        private void SaveImage_Click(object sender, RoutedEventArgs e)
+        {
+            BitmapSource currentWpfImage = MainImage.Source as BitmapSource;
+            if (currentWpfImage == null)
+            {
+                MessageBox.Show("Отсутсвует изображение");
+                return;
+            }
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "PNG Image (*.png)|*.png|JPEG Image (*.jpg)|*.jpg|Bitmap Image (*.bmp)|*.bmp";
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    Image<Bgr, byte> imageToSave = ToEmguImage(currentWpfImage);
+                    imageToSave.Save(saveFileDialog.FileName);
+
+                    MessageBox.Show($"Изображение успешно сохранено в {saveFileDialog.FileName}");
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show($"Ошибка! Не могу сохранить файл. Подробности: {ex.Message}");
+                }
+            }
+        }
+
+        private void UpdateImage_Click(object sender, RoutedEventArgs e)
+        {
+            BitmapSource currentWpfImage = MainImage.Source as BitmapSource;
+
+            if (currentWpfImage == null)
+            {
+                MessageBox.Show("Изображение отсутсвует");
+                return;
+            }
+
+            sourceImage = ToEmguImage(currentWpfImage);
+            MessageBox.Show("Изменения применены. Теперь это новый оригинал.");
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            if (sourceImage == null) return;
+            MainImage.Source = ToBitmapSource(sourceImage);
+        }
+
+        // --- Фильтры и эффекты ---
+
+        // Алгоритм выполняет масштабирование изображения, используя прямое преобразование.
+        private void OnGeometryFilterChanged(object sender, RoutedEventArgs e)
+        {
+            if (sourceImage == null) return;
+
+            int translateX = (int)BasicTranslateSliderX.Value;
+            int translateY = (int)BasicTranslateSliderY.Value;
+
+            Image<Bgr, byte> scaledImage = new Image<Bgr, byte>(sourceImage.Size);
+
+            for (int y_in = 0; y_in < sourceImage.Height; y_in++)
+            {
+                for (int x_in = 0; x_in < sourceImage.Width; x_in++)
+                {
+                    double x_out = x_in + translateX;
+                    double y_out = y_in + translateY;
+
+                    Bgr color = sourceImage[y_in, x_in];
+                    
+                    if (x_out >= 0 && x_out < sourceImage.Width && y_out >= 0 && y_out < sourceImage.Height)
+                    {
+                        scaledImage[(int)y_out, (int)x_out] = color;
+                    }
+                }
+            }
+
+            MainImage.Source = ToBitmapSource(scaledImage);
+        }
+    }
+}
